d4e24d1328a0b356e1b3c8f445cb6c8b56bf812c
Muhab <muhabme@gmail.com>
2023-06-20 08:14:00 +0300

    finished Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-
 import Hero from '@/components/Hero/Hero';
 
 export default function Home() {
     return (
-        <main className="flex-center">
+        <main className="mx-60 mt-16">
             <Hero />
         </main>
     );
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 export default function Hero() {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <div className="flex flex-col items-start justify-center min-h-[60vh] gap-4">
+            <h1 className="text-theme text-lg font-bold">Hi, my name is</h1>
+            <h2 className="text-giant text-lightest font-bold leading-[1.05]">
+                Muhab Gamal
+            </h2>
+            <h3 className="font-bold text-2xl text-lightest">
+                A Full Stack Web Developer
+            </h3>
+            <p className=" text-lg mr-[40%]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At,
+                deleniti iste, ipsam nostrum alias, minus dolorem dignissimos
+                placeat cupiditate animi ratione similique possimus earum optio!
+                Adipisci quo vitae ipsa aliquid?
+            </p>
+            <Link href={'/resume.pdf'} className='btn'>Check Out My Resume</Link>
+        </div>
+    );
 }
