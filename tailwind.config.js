fcbcb2861d4bbee663ed2c9f1e68ed32688555a1
Muhab <muhabme@gmail.com>
2023-06-20 06:26:11 +0300

    added style variables and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: '#0a192f',
+                dark: '#495670',
+                slate: '#8892b0',
+                light: '#ccd6f6',
+                lightest: '#a8b2d1',
+                lightnavy: '#112240',
+                lightestnavy: '#233554',
+                navyshadow: 'rgba(2, 12, 27, 0.7)',
+                theme: '#9d00ff',
+            },
+            fontSize: {
+                giant: 'clamp(40px, 8vw, 80px)',
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
