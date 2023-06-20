ab923785c184f744d5fb9aac970b7d8b038e3da9
Muhab <muhabme@gmail.com>
2023-06-20 07:53:11 +0300

    added hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Email from '@/components/Email/Email';
+import Hero from '@/components/Hero/Hero';
+import SocialIcons from '@/components/SocialIcons/SocialIcons';
 import Image from 'next/image';
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+        <main className="flex-center">
+            <SocialIcons />
+            <Hero />
+            <Email />
+        </main>
     );
 }
